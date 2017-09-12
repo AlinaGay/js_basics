9a09dbd906077bc2c5687085a95e27750dcca05f
AlinaGay <gayfullina.a@gmail.com>
2017-09-12 22:42:34 +0300

    added second day

--- first_day.js
+++
@@ -1,4 +0,0 @@
-console.log('hello world!');
-let x;
-x=1;
-console.log(x);
---
+++ second day.js
@@ -0,0 +1,108 @@
+/*let a = 3;
+let b = 4;
+let c = (a < b) ? 'a less than b' : 'b less than a';
+console.log(c);*/
+//2
+/*let c = 5 ? true : false;
+console.log(c);*/
+
+//3
+/*let c = 0 ? true : false;
+console.log(c);*/
+
+//4
+//console.log(3<4);
+
+//5
+//console.log(3>4);
+
+//6
+//console.log(4>=4);
+
+//7
+//console.log(3<=4);
+
+//8
+//console.log(5==5);
+
+//9
+//console.log(5=='5');
+
+//10
+//console.log(5==='5');
+
+//11
+//console.log(5!=5);
+
+//12
+//console.log(5!==6);
+
+//13
+//console.log(5 ? true : false);
+
+//14
+//console.log(0 ? true : false);
+
+//15
+//console.log("abc" ? true : false);
+
+//16
+// console.log("5" ? true : false);
+
+//17
+//console.log(3 && 4);
+
+//18
+//console.log(3 && 0);
+
+//19
+//console.log("" && 2)
+
+//20
+// console.log("" && 0)
+
+//21
+//console.log(3 || 4);
+
+//22
+//console.log(3||0);
+
+//23
+//console.log("" || 2);
+
+//24
+//console.log("" || 0);
+
+//25
+//console.log(!true);
+
+//26
+//let c = (1 / 3).toFixed(2);
+//console.log(c);
+
+//27
+//let c='hello'.length;
+//console.log(c);
+
+//28
+//let c='hello'.toUpperCase();
+//console.log(c);
+
+//29
+//let c='Hello, \n\tworld!';
+//console.log(c);
+
+//30
+//let c='Hello \'my\' world!';
+//console.log(c);
+
+//31
+//let c='Это \u2014\x20 просто строка';
+//console.log(c);
+
+//32
+let first = 'John';
+let last = 'Smith';
+// noinspection JSAnnotator
+let name = `${first} ${last}`;
+console.log(name);
