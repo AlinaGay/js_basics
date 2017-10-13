dd8463743d8de70464ed0b2a65710bffc768db89
AlinaGay <gayfullina.a@gmail.com>
2017-10-13 08:52:16 +0300

    added fifth_day

---
+++ fifth_day.js
@@ -0,0 +1,23 @@
+//42
+/*
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number(null));
+console.log(Number(undefined));
+*/
+
+//не работает
+//concole.log(String(true));
+//console.log(5+'');
+
+//43
+/*
+console.log(Boolean(""));
+console.log(Boolean('hello'));*/
+
+//44
+console.log(!5);
+console.log(!!5);
+console.log(Boolean(25));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
